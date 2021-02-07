be6bf2df48a69179f728233796d08ad7a46c9304
sunzeyu <1549522434@qq.com>
2021-02-07 22:58:55 +0800

    组件库页面的基本布局！

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,37 @@
 module.exports = {
     base: '/yz-web-doc/',
+
     title: '益政前端文档',
+
+    head: [
+        ['link', { rel: 'icon', href: '/favicons.ico' }]
+    ],
+
     description: 'Just playing around',
+
     themeConfig: { // 导航栏链接
         nav: [
             {text: 'Home', link: '/'},
-            {text: 'Guide', link: '/guide/'},
+            {
+                text: 'Docs',
+                items: [
+                    { text: 'specification', link: '/specification/' },
+                    { text: 'components', link: '/components/' }
+                ]
+            },
         ],
-        sidebar: [
-            '/',
-            '/page-a'
-        ]
+
+        sidebar: {
+            '/components/': [
+                '',
+                'searchTable'
+            ],
+
+            '/specification/': [
+
+            ],
+
+            '/': [''],
+        }
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,15 +1,14 @@
 ---
 home: true
-heroImage: /hero.png
+heroImage: /main.png
 heroText: 前端规范库 & 知识库
 tagline: 让团队开发更敏捷
 actionText: 从规范开始 →
-actionLink: /zh/guide/
+actionLink: /specification/
 features:
 - title: 规范
   details: 专注规范，团队开发轻松以维护。包括：命名规范、书写规范、Css规范、Js规范...
 - title: 组件化
   details: 构建符合产品的独立第三方组件库。加强组件跨用户,跨系统地使用。
-footer: MIT Licensed | Copyright © 2018-present Evan You
-sidebarDepth: 0
+footer: MIT Licensed | Copyright © 2021-OneGs
 ---
